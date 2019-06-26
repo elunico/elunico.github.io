26ced629688293b0343ddb46a5c4345616602559
thomaspovinelli <tompov227@gmail.com>
2019-06-25 23:24:36 -0400

    Shrunk images by default but clicking toggles size

--- js/progproj.js
+++ js/progproj.js
@@ -3,6 +3,17 @@ window.onload = () => {
   typeText('Programming Projects', '#mainHeader', () => {
     fade();
   });
+
+  let images = document.querySelectorAll('img');
+  for (let image of images) {
+    image.onclick = function () {
+      if (this.style.width == '50%' || !this.style.width) {
+        this.style.width = '100%';
+      } else if (this.style.width == '100%') {
+        this.style.width = '150%';
+      }
+    }
+  }
 }
 
 
