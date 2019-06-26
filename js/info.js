e737624b69d3095417be836c77ae85daaed593fc
thomaspovinelli <tompov227@gmail.com>
2019-06-25 20:28:41 -0400

    Added links on info and contact page

--- js/info.js
+++ js/info.js
@@ -1,8 +1,12 @@
 window.onload = () => {
   run_common();
+  const done = '<a class="headerLink" href="#information">Information</a> ' +
+    '&amp; <a class="headerLink" href="#content">Contact</a>';
 
   typeText('Information &amp; Contact', '#mainHeader', () => {
     fade();
+    let m = document.querySelector('#mainHeader');
+    m.innerHTML = done;
   });
 };
 
