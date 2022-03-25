dafda51b7f22df453f27bb2604afa0d44b695906
Tom <elunico@users.noreply.github.com>
2022-03-24 20:43:57 -0400

    Added pen ranking

---
+++ projects/pen-ranking/build.py
@@ -0,0 +1,44 @@
+import yaml
+
+with open("ranking.yaml") as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+
+print(data)
+
+template_file = '''<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <title>Tom's Official Pen Ranking</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  {}
+
+</body>
+
+</html>
+'''
+
+template_block = '''<div class="body-container">
+    <h1>{}</h1>
+    <div>
+      <ol>
+        {}
+      </ol>
+    </div>
+  </div>'''
+
+template_item = '''<li>{}</li>'''
+
+with open("index.html", "w") as f:
+    block = ''
+    for key in data:
+        block += template_block.format(key, "\n".join([template_item.format(item) for item in data[key]]))
+
+    f.write(template_file.format(block))
