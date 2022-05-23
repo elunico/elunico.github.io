e203f2cde99870c221923c5a36033372573d11ae
Tom <elunico@users.noreply.github.com>
2022-05-23 12:17:03 -0400

    added query params

--- projects/grade-calc/index.js
+++ projects/grade-calc/index.js
@@ -1,20 +1,27 @@
 const totalPointsDiv = document.querySelector('#total-points');
 const sepDiv = document.querySelector('#sep');
-const missing = document.querySelector('#missing-tally')
-const result = document.querySelector('#result')
+const missing = document.querySelector('#missing-tally');
+const result = document.querySelector('#result');
+
+let params = new URLSearchParams(window.location.search);
+let totalPoints = Number(params.get('tot')) || 0;
+let sep = params.get('sep') || '';
+
+totalPointsDiv.innerHTML = totalPoints;
+sepDiv.innerHTML = sep;
 
 document.addEventListener('keydown', event => {
     if (event.key == 'Enter') {
-        missing.value = ''
-        event.stopImmediatePropagation()
+        missing.value = '';
+        event.stopImmediatePropagation();
     }
 });
 
 missing.addEventListener('input', event => {
     let total_points = parseInt(totalPointsDiv.value);
-    let sep = sepDiv.value
+    let sep = sepDiv.value;
 
     let points = missing.value.trim().split(sep).map((val, index) => Number(val.trim())).reduce((acc, c) => acc + c, 0);
-    result.textContent = `score: ${total_points - points}`
-})
+    result.textContent = `score: ${total_points - points}`;
+});
 
