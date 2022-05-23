3370cab59f7f97182cf4f9a97364defc6d80fd63
Tom <elunico@users.noreply.github.com>
2022-05-23 12:22:05 -0400

    Fixed names

--- projects/grade-calc/index.js
+++ projects/grade-calc/index.js
@@ -1,5 +1,5 @@
-const totalPointsDiv = document.querySelector('#total-points');
-const sepDiv = document.querySelector('#sep');
+const totPointsInput = document.querySelector('#total-points');
+const sepInput = document.querySelector('#sep');
 const missing = document.querySelector('#missing-tally');
 const result = document.querySelector('#result');
 
@@ -7,8 +7,8 @@ let params = new URLSearchParams(window.location.search);
 let totalPoints = Number(params.get('tot')) || 0;
 let sep = params.get('sep') || '';
 
-totalPointsDiv.innerHTML = totalPoints;
-sepDiv.innerHTML = sep;
+totPointsInput.value = totalPoints;
+sepInput.value = sep;
 
 document.addEventListener('keydown', event => {
     if (event.key == 'Enter') {
@@ -18,8 +18,8 @@ document.addEventListener('keydown', event => {
 });
 
 missing.addEventListener('input', event => {
-    let total_points = parseInt(totalPointsDiv.value);
-    let sep = sepDiv.value;
+    let total_points = parseInt(totPointsInput.value);
+    let sep = sepInput.value;
 
     let points = missing.value.trim().split(sep).map((val, index) => Number(val.trim())).reduce((acc, c) => acc + c, 0);
     result.textContent = `score: ${total_points - points}`;
