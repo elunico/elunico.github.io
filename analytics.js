e9da24b9cc000ad3a34da17fd9d8e9b06c1873e3
Tom <elunico@users.noreply.github.com>
2022-09-25 01:41:09 -0400

    Added some analytics and transparency

---
+++ analytics.js
@@ -0,0 +1,13 @@
+function sendAnalytics() {
+    let arrivalTime = Date.now();
+    document.addEventListener('visibilitychange', function logData() {
+        if (document.visibilityState === 'hidden') {
+            navigator.sendBeacon('https://analytics.eluni.co/visit', JSON.stringify({
+                elapsed: Date.now() - arrivalTime,
+                source: window.location.pathname
+            }));
+        }
+    });
+}
+
+sendAnalytics();
