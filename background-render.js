ab90a2e891b232bc48502d5d1534e1fe970681ae
Tom <elunico@users.noreply.github.com>
2022-10-03 11:25:43 -0400

    Updated background script with skip nav

--- background-render.js
+++ background-render.js
@@ -8,14 +8,34 @@ async function randomBinaryString() {
   return Array(12000).fill().map(() => (Math.random() < 0.55) ? '0' : '1').join('');
 }
 
+function skipper() {
+  let background = document.getElementsByClassName('bg')[0];
+  console.log(background);
+  if (background) {
+    document.body.removeChild(background);
+  }
+  window.location = '/#main'
+}
+
 async function fillBackground() {
   return randomBinaryString().then(text => {
-    const background = document.querySelector('.bg');
+    const skipper = document.createElement('a');
+    skipper.textContent = 'Skip Navigation';
+    skipper.classList.add('skip-nav');
+    skipper.setAttribute('tabindex', '1');
+    skipper.href = 'javascript:skipper()'
+    document.body.prepend(skipper);
+
+    const background = document.createElement('div');
+    background.setAttribute('aria-hidden', "true");
+    background.classList.add('bg');
     background.textContent = text;
+    document.body.appendChild(background);
 
     let count = 0;
     while (true) {
       let rects = background.getClientRects();
+      console.log(rects);
       if (rects.length <= 0) {
         break;
       }
@@ -34,8 +54,11 @@ async function fillBackground() {
       count++;
     }
 
-
-
+    const remover = document.createElement('button');
+    remover.textContent = 'Remove Background'
+    remover.id = 'remove-button';
+    remover.onclick = event => document.body.removeChild(background) && document.body.removeChild(remover);
+    document.body.appendChild(remover)
   });
 }
 
