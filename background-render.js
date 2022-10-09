acffddb41bddce576af803d1efca70bdec171981
Tom <elunico@users.noreply.github.com>
2022-10-09 01:19:26 -0400

    Changed some code to be friendly to older browsers

--- background-render.js
+++ background-render.js
@@ -3,25 +3,32 @@ let html = document.documentElement;
 
 function skipper() {
     window.location = './#main';
-    const main = document.querySelector('main');
+    var main = document.getElementsByTagName('main')[0];
     main.setAttribute('tabindex', '0');
     main.focus();
 }
 
-async function fillBackground() {
-    const skipper = document.createElement('a');
-    skipper.textContent = 'Skip Navigation';
-    skipper.classList.add('skip-nav');
-    skipper.setAttribute('tabindex', '1');
-    skipper.href = 'javascript:skipper()'
-    document.body.prepend(skipper);
+function fillBackground(then) {
+    try {
+        var skipper = document.createElement('a');
+        skipper.textContent = 'Skip Navigation';
+        skipper.classList.add('skip-nav');
+        skipper.setAttribute('tabindex', '1');
+        skipper.href = 'javascript:skipper()'
+        document.body.prepend(skipper);
 
-    const remover = document.createElement('button');
-    remover.innerHTML = '<span>Remove Background</span>'
-    remover.id = 'remove-button';
-    remover.onclick = event => (document.body.style.background = "var(--page-background-color)") && document.body.removeChild(remover);
-    document.body.appendChild(remover)
+        var remover = document.createElement('button');
+        remover.innerHTML = '<span>Remove Background</span>'
+        remover.id = 'remove-button';
+        remover.onclick = event => (document.body.style.background = "var(--page-background-color)") && document.body.removeChild(remover);
+        document.body.appendChild(remover)
+    } catch (err) {
+        console.error("Failed to load background:")
+        console.error(err);
+        return;
+    }
+    then();
 }
 
-fillBackground().then(() => console.log("Backgrounded"));
+fillBackground(() => console.log("Backgrounded"));
 
--- image-loader.js
+++ image-loader.js
@@ -1,36 +1,55 @@
 function loadImages() {
-  let images = document.querySelectorAll('img');
+    let images = document.querySelectorAll('img');
 
 
-  function modeMatches(dataMode, isDark) {
-    return ((dataMode == 'dark' && isDark) || (dataMode == 'light' && !isDark));
-  }
+    function modeMatches(dataMode, isDark) {
+        return ((dataMode == 'dark' && isDark) || (dataMode == 'light' && !isDark));
+    }
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (window.IntersectionObserver) {
+        const observer = new IntersectionObserver((entries, observer) => {
+            let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    console.log("Calling back");
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        let elt = entry.target;
+            console.log("Calling back");
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    let elt = entry.target;
 
-        if (
-          (elt.getAttribute('data-mode') === 'all' ||
-            modeMatches(elt.getAttribute('data-mode'), darkMode)) &&
-          elt.getAttribute('data-loaded') !== 'true'
-        ) {
-          elt.src = elt.getAttribute('data-src');
-          elt.setAttribute('data-loaded', true);
-          // observer.unobserve(elt);
+                    if (
+                        (elt.getAttribute('data-mode') === 'all' ||
+                            modeMatches(elt.getAttribute('data-mode'), darkMode)) &&
+                        elt.getAttribute('data-loaded') !== 'true'
+                    ) {
+                        elt.src = elt.getAttribute('data-src');
+                        elt.setAttribute('data-loaded', true);
+                        // observer.unobserve(elt);
+                    }
+
+
+                }
+            });
+        });
+        images.forEach(img => observer.observe(img));
+    } else {
+        function loadVisibleImages() {
+            for (var i = 0; i < images.length; i++) {
+                if (!images[i].hasAttribute('data-src')) continue;
+
+                var rect = images[i].getBoundingClientRect();
+                console.log(rect.x, window.scrollX, rect.y, window.scrollY, innerWidth, innerHeight)
+                if (rect.x < (window.scrollX + innerWidth) && rect.y < (window.scrollY + innerHeight)) {
+                    images[i].src = images[i].getAttribute('data-src');
+                }
+            }
         }
 
+        window.addEventListener('scroll', event => {
+            loadVisibleImages();
+        });
+        // once before scrolling
+        loadVisibleImages();
+    }
 
-      }
-    });
-  });
-
-  console.log(images);
-  images.forEach(img => observer.observe(img));
 }
 
 loadImages();
