9b5b2c6b4dba94d520aac3053cb8c6e70da5c5cb
Tom <elunico@users.noreply.github.com>
2022-10-30 21:25:54 -0400

    Removed remove background button

--- background-render.js
+++ background-render.js
@@ -14,16 +14,16 @@ function fillBackground(then) {
         skipper.textContent = 'Skip Navigation';
         skipper.classList.add('skip-nav');
         skipper.setAttribute('tabindex', '1');
-        skipper.href = 'javascript:skipper()'
-        document.body.insertBefore(skipper, document.body.firstElementChild)
+        skipper.href = 'javascript:skipper()';
+        document.body.insertBefore(skipper, document.body.firstElementChild);
 
-        var remover = document.createElement('button');
-        remover.innerHTML = '<span>Remove Background</span>'
-        remover.id = 'remove-button';
-        remover.onclick = event => (document.body.style.background = "var(--page-background-color)") && document.body.removeChild(remover);
-        document.body.appendChild(remover)
+        // var remover = document.createElement('button');
+        // remover.innerHTML = '<span>Remove Background</span>'
+        // remover.id = 'remove-button';
+        // remover.onclick = event => (document.body.style.background = "var(--page-background-color)") && document.body.removeChild(remover);
+        document.body.appendChild(remover);
     } catch (err) {
-        console.error("Failed to load background:")
+        console.error("Failed to load background:");
         console.error(err);
         return;
     }
