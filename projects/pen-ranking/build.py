49aefa9f14985cacd08a2f0a5cc6000642ee65b1
elunico <elunico@users.noreply.github.com>
2025-09-01 21:02:13 -0400

    updating rankings

--- projects/pen-ranking/build.py
+++ projects/pen-ranking/build.py
@@ -1,56 +1,53 @@
 import yaml
 
-with open("ranking.yaml") as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-
-print(data)
 
 template_file = '''<!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
   <title>Tom's Official Pen Ranking</title>
   <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-
   {}
-
 </body>
-
 </html>
 '''
 
-template_ol_block = '''<div class="body-container">
-    <h1>{}</h1>
-    <div>
-      <ol>
-        {}
-      </ol>
-    </div>
-  </div>'''
-
-template_ul_block = '''<div class="body-container">
+template_list_block = '''<div class="body-container">
   <h1>{}</h1>
   <div>
-    <ul>
+    <%sl>
       {}
-    </ul>
+    </%sl>
   </div>
 </div>'''
 
 template_item = '''<li>{}</li>'''
 
-with open("index.html", "w") as f:
-    block = ''
-    for key in data:
-        if 'unordered' in key:
-            block += template_ul_block.format(key, ''.join([template_item.format(item) for item in data[key]]))
-        else:
-            block += template_ol_block.format(key, "\n".join([template_item.format(item) for item in data[key]]))
 
-    f.write(template_file.format(block))
+def format_item(item):
+    return template_item.format(item.replace('\n', '<br/>').replace('  ', '&nbsp;&nbsp;'))
+
+
+def format_block(key, items):
+    list_block = template_list_block % (
+        tuple([('u' if 'unordered' in key else 'o')])*2)
+
+    return list_block.format(key, "\n".join([format_item(item) for item in items]))
+
+
+def main():
+    with open("ranking.yaml") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    with open("index.html", "w") as f:
+        file = ''.join(format_block(key, data[key]) + '\n\n' for key in data)
+        f.write(template_file.format(file))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
