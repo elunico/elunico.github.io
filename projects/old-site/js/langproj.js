85fdbd4c789431e5355e7c1de6e255d0d89b4434
elunico <elunico@users.noreply.github.com>
2025-10-09 21:54:34 -0400

    tore down website for now

--- projects/old-site/js/langproj.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  run_common_before_type();
-  typeText('Language Projects', '#mainHeader')
-    .then(() => {
-      run_common_after_type();
-    });
-};
--- projects/old-site/js/vimhelp.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  run_common_before_type();
-  typeText('Commands: Help', '#mainHeader')
-    .then(() => {
-      run_common_after_type();
-    });
-};
